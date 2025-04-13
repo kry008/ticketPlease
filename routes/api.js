8ebf6c69d0ea84c5c2c4cb8c48edd3f626343a51
kry008 <git@mail.kry008.xyz>
2025-04-13 21:58:42 +0200

    Preparation for checking

--- routes/api.js
+++ routes/api.js
@@ -37,4 +37,35 @@ router.all('/', (req, res) => {
     res.status(202).json({ message: 'API is working' });
 })
 
+router.get('/settings', async (req, res) => {
+    try {
+        const [rows] = await con.execute('SELECT * FROM settings WHERE name = "siteName"');
+        return res.status(200).json(rows);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+router.post('/checkTicket', async (req, res) => {
+    var ticket = req.body.code
+    var pass = ticket.split('?pass=')[1]
+    var id = ticket.split('ticket://')[1].split('?pass=')[0]
+    console.log('Ticket:', ticket)
+    console.log('ID:', id)
+    console.log('Pass:', pass)
+    const [rows] = await con.execute("SELECT `tickets`.`id` AS 'idTicket', `tickets`.`name` AS 'ticketHolder', `tickets`.`email` AS 'email', `tickets`.`active` AS 'active', `ticketTypes`.`name` AS 'ticketType', `ticketTypes`.`date` AS 'date', COUNT(`ticketCheck`.`id`) AS 'checkCount' FROM `tickets` JOIN `ticketTypes` ON `tickets`.`type` = `ticketTypes`.`id` LEFT JOIN `ticketCheck` ON `tickets`.`id` = `ticketCheck`.`ticketId` WHERE `tickets`.id = ? AND tickets.pass = ? GROUP BY `tickets`.`id`; ", [id, pass])
+    console.log('Rows:', rows);
+    if(rows.length > 0)
+    {
+        await con.execute('INSERT INTO ticketCheck (ticketId, userId) VALUES (?, ?)', [rows[0].idTicket, req.session.user.id])
+        return res.status(200).json({ message: 'Ticket checked', data: rows[0] });
+    }
+    else
+    {
+        return res.status(404).json({ message: 'Ticket not found' });
+    }
+        
+});
+
 export default router
